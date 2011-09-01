7a2b0fe351cb1fb62d787f140008eb1687b42d75
gkerns <gkerns@ysu.edu>
2011-09-01 18:20:16 -0400

    so many, many changes

---
+++ prelim.R
@@ -0,0 +1,29 @@
+
+# initial customizations
+seed <- 42
+set.seed(seed)
+options(width = 60)
+options(useFancyQuotes = FALSE)
+
+
+# load packages needed by the book
+library(actuar)
+library(aplpack)
+library(boot)
+library(coin)
+library(combinat)
+library(distrEx)
+library(e1071)
+library(ggplot2)
+library(HH)
+library(Hmisc)
+library(lattice)
+library(lmtest)
+library(mvtnorm)
+library(prob)
+library(qcc)
+library(RcmdrPlugin.IPSUR)
+library(reshape)
+library(scatterplot3d)
+library(stats4)
+library(TeachingDemos)
