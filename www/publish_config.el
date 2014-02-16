0c1183e8badb82b2aa32ed104b5c5f0f8f920c4b
G. Jay Kerns <gkerns@ysu.edu>
2014-02-16 10:47:52 -0500

    added website stuff to Savannah www directory

---
+++ www/publish_config.el
@@ -0,0 +1,33 @@
+
+(defvar org-dist "~/git/org-mode")
+(add-to-list 'load-path (concat org-dist "/lisp"))
+(add-to-list 'load-path (concat org-dist "/contrib/lisp"))
+(require 'org)
+(require 'ox-publish)
+
+(setq org-publish-project-alist
+      '(;; These are the main web files
+	("ipsurweb-notes"
+	 :base-directory "~/gitm/ipsurweb/" ;; Change this to your local dir
+	 :base-extension "org"
+	 :publishing-directory "output"
+	 :recursive t
+	 :publishing-function org-html-publish-to-html
+	 :headline-levels 4             ; Just the default for this project.
+	 :auto-preamble t
+	 :section-numbers nil
+	 :headline-levels 3
+	 :table-of-contents nil
+	 :html-doctype "html5"
+	 :html-head "<link rel='stylesheet' type='text/css' href='css/style.css' />"
+	 :html-head-include-default-style nil)
+	
+	;; These are static files (images, pdf, etc)
+	("ipsurweb-static"
+	 :base-directory "~/gitm/ipsurweb/" ;; Change this to your local dir
+	 :base-extension "css\\|js\\|png\\|jpg\\|ico\\|gif\\|pdf\\|mp3\\|ogg\\|swf\\|txt\\|asc"
+	 :publishing-directory "output"
+	 :recursive t
+	 :publishing-function org-publish-attachment)	
+	("ipsurweb" :components ("ipsurweb-notes" "ipsurweb-static"))))
+
