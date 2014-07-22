58568f32d71111202a14379fbde0ff9b2f3a0816
G. Jay Kerns <gkerns@ysu.edu>
2014-07-22 09:21:53 -0400

    fixes to gendata and gitignore

--- pkg/R/genData.R
+++ pkg/R/genData.R
@@ -1,4 +1,8 @@
+####################################
+# Functions to generate data
+# for exercises or quizzes
 
+# continuous X data
 
 genXdata <- function(n, ncol = 1,
                      mu = rep(0, ncol),
@@ -15,8 +19,8 @@ genXdata <- function(n, ncol = 1,
   tmp
 }
 
-
-a = genXdata(50, ncol = 3, roundto = 2)
+genXdata(10, ncol = 3, roundto = 2)
+X = genXdata(10, ncol = 3, roundto = 2)
 
 #######################################################
 # logistic regression data
@@ -27,13 +31,13 @@ genLogRegData <- function(xdata,
                          ){
   tmp <- as.matrix(xdata) %*% beta
   probs <- exp(tmp)/(1 + exp(tmp))
-  y <- apply(x, 1, function(p){rbinom(1, size = 1, prob = p)})
+  y <- apply(probs, 1, function(p){rbinom(1, size = 1, prob = p)})
   xdata[ , yname] <- y
   xdata
 }
 
 
-genLogRegData(a, beta = c(1,2,3))
+genLogRegData(X, beta = c(1,2,3))
 
 
 ######################################################3
@@ -45,8 +49,7 @@ genIndepTable <- function(n = sample(100:500, size = 1),
                                          Y = paste("y", 1:length(pcol), sep = "")),
                           nfixed = FALSE,
                           addmargins = TRUE,
-                          as.df = FALSE, untable = TRUE
-                          ){
+                          as.df = FALSE, untable = TRUE){
   prow <- prow/sum(prow)
   pcol <- pcol/sum(pcol)
   pmatrix <- outer(prow, pcol)
@@ -97,5 +100,68 @@ genIndepTable(n = 100, nfixed = TRUE, as.df = TRUE, untable = FALSE)
 
 
 A = genIndepTable(n = 500, nfixed = TRUE, as.df = TRUE)
-chisq.test(xtabs(~X + Y, data = A))
+chisq.test(xtabs(~., data = A))
+
+
+
+######################################################3
+# general two-way tables
+
+gen2wayTable <- function(n = sample(100:500, size = 1),
+                          pmatrix = matrix(1:12, nrow = 3),
+                          dmnames = list(X = paste("x", 1:nrow(pmatrix), sep = ""),
+                                         Y = paste("y", 1:ncol(pmatrix), sep = "")),
+                          nfixed = FALSE,
+                          addmargins = TRUE,
+                          as.df = FALSE, untable = TRUE){
+  pmatrix <- pmatrix/sum(pmatrix)
+  f <- function(x) rbinom(1, size = n, prob = x)
+  
+  flag <- FALSE
+  while(!flag){
+    tmp <- matrix(sapply(pmatrix, f, simplify = "array"), nrow = nrow(pmatrix)) 
+    if (sum(tmp) >= n) flag <- TRUE  # generated enough
+  }
+  
+  dimnames(tmp) <- dmnames
+  tmp <- as.table(tmp)
+  
+  if (nfixed && (sum(tmp) > n)){
+    # gotta get rid of extra rows
+    tmp <- as.data.frame(tmp)
+    require(reshape)
+    tmp <- with(tmp, untable(tmp, Freq))
+    tmp[ , "Freq"] <- NULL
+    tmp <- tmp[sample(1:dim(tmp)[1], size = n, replace = FALSE),]
+    rownames(tmp) <- 1:n
+    tmp <- xtabs(formula = ~., data = tmp)
+  }
+  
+  if (as.df){
+    tmp <- as.data.frame(tmp)
+    if (untable){
+      require(reshape)
+      tmp <- with(tmp, untable(tmp, Freq))
+      tmp[ , "Freq"] <- NULL
+      rownames(tmp) <- 1:dim(tmp)[1]
+    }
+    tmp
+  } else if (addmargins) {
+    addmargins(tmp)
+  } else {
+    tmp
+  }
+}
+
+
+gen2wayTable(n = 100)
+gen2wayTable(n = 100, nfixed = TRUE)
+gen2wayTable(n = 100, nfixed = TRUE, as.df = TRUE)
+gen2wayTable(n = 100, nfixed = TRUE, as.df = TRUE, untable = FALSE)
+
+w = matrix(c(8, 5, 3, 2, 5, 5), nrow = 2)
+
+B = gen2wayTable(n = 300, pmatrix = w, addmargins = FALSE)
+chisq.test(B)
+
 
