aed2f0e44c5a479ecfd9ddc66e921be99d0d4a6a
G. Jay Kerns <gkerns@ysu.edu>
2014-07-22 08:41:57 -0400

    initial commit of genData.R

---
+++ pkg/R/genData.R
@@ -0,0 +1,101 @@
+
+
+genXdata <- function(n, ncol = 1,
+                     mu = rep(0, ncol),
+                     Sigma = diag(ncol),
+                     varnames = paste("x", 1:ncol, sep = ""),
+                     roundto = NULL
+                     ){
+  require(MASS)
+  tmp <- as.data.frame(mvrnorm(n, mu = mu, Sigma = Sigma))
+  names(tmp) <- varnames
+  if (!is.null(roundto)){
+    tmp <- round(tmp, roundto)
+  }
+  tmp
+}
+
+
+a = genXdata(50, ncol = 3, roundto = 2)
+
+#######################################################
+# logistic regression data
+
+genLogRegData <- function(xdata,
+                         beta = rep(1, ncol(xdata)),
+                         yname = "y"
+                         ){
+  tmp <- as.matrix(xdata) %*% beta
+  probs <- exp(tmp)/(1 + exp(tmp))
+  y <- apply(x, 1, function(p){rbinom(1, size = 1, prob = p)})
+  xdata[ , yname] <- y
+  xdata
+}
+
+
+genLogRegData(a, beta = c(1,2,3))
+
+
+######################################################3
+# contingency tables
+
+genIndepTable <- function(n = sample(100:500, size = 1),
+                          prow = 1:3, pcol = 1:4,
+                          dmnames = list(X = paste("x", 1:length(prow), sep = ""),
+                                         Y = paste("y", 1:length(pcol), sep = "")),
+                          nfixed = FALSE,
+                          addmargins = TRUE,
+                          as.df = FALSE, untable = TRUE
+                          ){
+  prow <- prow/sum(prow)
+  pcol <- pcol/sum(pcol)
+  pmatrix <- outer(prow, pcol)
+  f <- function(x) rbinom(1, size = n, prob = x)
+  
+  flag <- FALSE
+  while(!flag){
+    tmp <- matrix(sapply(pmatrix, f, simplify = "array"), nrow = length(prow)) 
+    if (sum(tmp) >= n) flag <- TRUE  # generated enough
+  }
+  
+  dimnames(tmp) <- dmnames
+  tmp <- as.table(tmp)
+  
+  if (nfixed && (sum(tmp) > n)){
+    # gotta get rid of extra rows
+    tmp <- as.data.frame(tmp)
+    require(reshape)
+    tmp <- with(tmp, untable(tmp, Freq))
+    tmp[ , "Freq"] <- NULL
+    tmp <- tmp[sample(1:dim(tmp)[1], size = n, replace = FALSE),]
+    rownames(tmp) <- 1:n
+    tmp <- xtabs(formula = ~., data = tmp)
+  }
+
+  if (as.df){
+    tmp <- as.data.frame(tmp)
+    if (untable){
+      require(reshape)
+      tmp <- with(tmp, untable(tmp, Freq))
+      tmp[ , "Freq"] <- NULL
+      rownames(tmp) <- 1:dim(tmp)[1]
+    }
+    tmp
+  } else if (addmargins) {
+    addmargins(tmp)
+  } else {
+    tmp
+  }
+}
+  
+
+genIndepTable(n = 100)
+genIndepTable(n = 100, nfixed = TRUE)
+genIndepTable(n = 100, nfixed = TRUE, as.df = TRUE)
+genIndepTable(n = 100, nfixed = TRUE, as.df = TRUE, untable = FALSE)
+
+
+
+A = genIndepTable(n = 500, nfixed = TRUE, as.df = TRUE)
+chisq.test(xtabs(~X + Y, data = A))
+
