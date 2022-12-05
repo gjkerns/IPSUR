4c75c4f00d011b86d059f843b619372da92b8c85
G. Jay Kerns <gkerns@ysu.edu>
2022-12-05 11:51:48 -0500

    text updates

---
+++ renv/library/R-4.2/x86_64-w64-mingw32/renv/doc/bioconductor.R
@@ -0,0 +1,20 @@
+## ---- include = FALSE---------------------------------------------------------
+knitr::opts_chunk$set(
+  collapse = TRUE,
+  comment  = "#>",
+  eval     = FALSE
+)
+
+## -----------------------------------------------------------------------------
+#  # use the latest-available Bioconductor release
+#  renv::init(bioconductor = TRUE)
+#  
+#  # use a specific version of Bioconductor
+#  renv::init(bioconductor = "3.14")
+
+## -----------------------------------------------------------------------------
+#  renv::settings$bioconductor.version("3.14")
+
+## -----------------------------------------------------------------------------
+#  options(renv.bioconductor.repos = c(...))
+
